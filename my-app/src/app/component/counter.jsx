97f69f4bd5147282163f9fc420d787facfa9eeb1
Basit Ali <opoali911@gmail.com>
2025-04-07 15:28:34 +0500

    add more functionality in couter function on the decrement

--- my-app/src/app/component/counter.jsx
+++ my-app/src/app/component/counter.jsx
@@ -5,9 +5,14 @@ import  { useState } from 'react'
 
 const Counter = () => {
     const [count,setCount]=useState(0)
+    const decrement=()=>{
+        if(count>0){
+            setCount(count-1)
+        }
+    }
   return (
     <div className='w-72 h-72 bg-sky-400  flex justify-center items-center gap-10 rounded-xl shadow-xl hover:bg-emerald-500 transition-all'>
-        <Minus className='w-12 h-12 rounded-full cursor-pointer bg-red-500 p-2  hover:bg-slate-300' onClick={()=>{setCount(count-1)}}/>
+        <Minus className='w-12 h-12 rounded-full cursor-pointer bg-red-500 p-2  hover:bg-slate-300' onClick={decrement}/>
         <strong className='text-3xl'>{count}</strong>
         <Plus className='w-12 h-12 rounded-full cursor-pointer bg-red-500 p-2 hover:bg-slate-300' onClick={()=>{setCount(count+1)}}/>
 
