730a7ecf7e7ad09ae4447fc1bafd040df0c32d80
Basit Ali <opoali911@gmail.com>
2025-06-22 19:34:56 +0500

    exploring new thing during explore how to handle errors

--- practice/src/app/all/error.jsx
+++ practice/src/app/all/error.jsx
@@ -1,20 +1,26 @@
-'use client'
-export default function Error({error,reset}) {
-  console.dir(error)
-  console.log("Digest Value: ",error.digest)
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Error({ error, reset }) {
+  console.dir(error);
+  console.log("Digest Value: ", error.digest);
+  const router = useRouter();
   return (
     <div>
-        <h1>Error page</h1>
-        <p>Name:{error.name}</p>
-        <p>Message: {error.message}</p> 
-        <p>Stack: {error.stack}</p>
-        <p>Digest: {error.digest}</p>
-        <button
-        onClick={() => reset()}
+      <h1>Error page</h1>
+      <p>Name:{error.name}</p>
+      <p>Message: {error.message}</p>
+      <p>Stack: {error.stack}</p>
+      <p>Digest: {error.digest}</p>
+      <button
+        onClick={() => {
+          reset();
+        }}
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-400 cursor-pointer"
       >
         Try Again
       </button>
     </div>
-  )
+  );
 }
