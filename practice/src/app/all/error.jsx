9b80e64e4b656e41edc6ffa96123f87b89eb4d4c
Basit Ali <opoali911@gmail.com>
2025-06-22 19:36:04 +0500

    explore new method in react which is startTransition

--- practice/src/app/all/error.jsx
+++ practice/src/app/all/error.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { startTransition } from "react";
 
 export default function Error({ error, reset }) {
   console.dir(error);
@@ -15,7 +16,11 @@ export default function Error({ error, reset }) {
       <p>Digest: {error.digest}</p>
       <button
         onClick={() => {
-          reset();
+         startTransition(()=>{
+           reset();
+          router.refresh()
+         })
+
         }}
         className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-400 cursor-pointer"
       >
