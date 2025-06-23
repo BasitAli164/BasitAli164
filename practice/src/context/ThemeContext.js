e1161b1c6ef1345d91a9681c8c88526ef3cd8e40
Basit Ali <opoali911@gmail.com>
2025-06-23 08:26:55 +0500

    optimize the code

--- practice/src/context/ThemeContext.js
+++ practice/src/context/ThemeContext.js
@@ -3,11 +3,15 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(
+    // localStorage.getItem("isDark")==="false"?false:true
+        localStorage.getItem("isDark")==="true"
+  );
   function toggle() {
     setIsDark((prev) => !prev);
   }
   useEffect(() => {
+    localStorage.setItem("isDark",isDark)
     isDark
       ? document.documentElement.classList.add("dark")
       : document.documentElement.classList.remove("dark");
