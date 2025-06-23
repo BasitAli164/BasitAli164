bb157b07f1b21a0f155da6082e8e23a0635ba444
Basit Ali <opoali911@gmail.com>
2025-06-23 14:32:30 +0500

    explore the rendering concept in next js

--- practice/src/app/(services)/products/[id]/page.jsx
+++ practice/src/app/(services)/products/[id]/page.jsx
@@ -1,10 +1,21 @@
-
-export default async function ProductDetails({params}) {
-    const {id}=await params;
-    console.log("id :",id)
+export default async function ProductDetails({ params }) {
+  const { id } = await params;
+  console.log("id :", id);
   return (
     <div>
-      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae eligendi illum debitis, cum laborum totam voluptatibus enim doloremque itaque alias nostrum? Culpa obcaecati accusantium eum magnam, nobis et alias, eligendi aliquid maiores porro hic molestiae atque reprehenderit commodi officia deleniti eaque aliquam reiciendis enim, optio ducimus inventore autem ipsum non! Sit hic commodi, iure laboriosam labore dicta quod ducimus! Earum dicta optio sed tenetur quas quos accusantium doloribus, minima perspiciatis placeat distinctio quia aliquam itaque quasi incidunt? Quidem repellendus iusto nihil unde eos voluptate nemo porro quod, eveniet totam non, quo a nobis libero dolores eligendi! Odit praesentium sint corporis?</h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae eligendi
+        illum debitis, cum laborum totam voluptatibus enim doloremque itaque
+        alias nostrum? Culpa obcaecati accusantium eum magnam, nobis et alias,
+        eligendi aliquid maiores porro hic molestiae atque reprehenderit commodi
+        officia deleniti eaque aliquam reiciendis enim, optio ducimus inventore
+        autem ipsum non! Sit hic commodi, iure laboriosam labore dicta quod
+        ducimus! Earum dicta optio sed tenetur quas quos accusantium doloribus,
+        minima perspiciatis placeat distinctio quia aliquam itaque quasi
+        incidunt? Quidem repellendus iusto nihil unde eos voluptate nemo porro
+        quod, eveniet totam non, quo a nobis libero dolores eligendi! Odit
+        praesentium sint corporis?
+      </h1>
     </div>
-  )
+  );
 }
--- practice/src/app/about/[...id]/page.jsx
+++ practice/src/app/about/[...id]/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function CatchAll() {
   return (
     <div>
-      <h1 className='text-6xl'>Catch All Routes</h1>
+      <h1 className="text-6xl">Catch All Routes</h1>
     </div>
-  )
+  );
 }
