d61bee4ed2dae1c70d95cf3a0efc8c241354d035
Basit Ali <opoali911@gmail.com>
2025-06-23 08:31:06 +0500

    facing error and try to fix them

--- practice/src/app/layout.js
+++ practice/src/app/layout.js
@@ -1,3 +1,4 @@
+import ThemeProvider from "@/context/ThemeContext";
 import "./globals.css";
 
 export const metadata={
@@ -11,7 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <ThemeProvider>
         {children}
+
+        </ThemeProvider>
       </body>
     </html>
   );
