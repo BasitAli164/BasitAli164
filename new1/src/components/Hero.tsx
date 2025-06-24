7d5eb4eb3c6d81adcfd7546c985693f6d09150df
Basit Ali <opoali911@gmail.com>
2025-06-24 12:34:15 +0500

    fixing bugs in header

--- new1/src/components/Hero.tsx
+++ new1/src/components/Hero.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import heroImage from '../../public/images/header.jpg'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='w-full h-screen' style={{background:"url(../../public/images/header.jpg)"}}>
+        <h1>Dramatically Engage</h1>
       
     </div>
   )
