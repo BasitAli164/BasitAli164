818c872f217ed8f5ac772390452870a830cfee90
Basit Ali <opoali911@gmail.com>
2025-06-24 13:09:56 +0500

    create the advertise component also

--- new1/src/components/advertise.tsx
+++ new1/src/components/advertise.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import { Apple } from "lucide-react";
 
 const Advertise = () => {
   return (
-    <div>
-        
-      
-    </div>
-  )
-}
+    <div className="w-full bg-[yellowgreen] p-8 sm:p-16 md:p-20 lg:p-28 flex flex-col justify-center items-center gap-4 ">
+      <Apple className="text-white size-14 sm:size-20"  />
+      <h2 className="text-lg sm:xl md:text-3xl text-white font-semibold text-center tracking-[1px] ">Completely synergize resource taxing relationships</h2>
 
-export default Advertise
+      <p className="text-sm  md:text-lg text-white opacity-70    text-center">
+        Professionally cultivate one-to-one customer service with robust ideas.
+        Dynamically innovate resource-leveling customer service for state of the
+        art customer service.
+      </p>
+    </div>
+  );
+};
+
+export default Advertise;
