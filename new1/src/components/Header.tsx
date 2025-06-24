e3dd0bcb707263cd19a979ffbf354e958586f636
Basit Ali <opoali911@gmail.com>
2025-06-24 12:34:36 +0500

    successfully add the bg image of the hero page

--- new1/src/components/Header.tsx
+++ new1/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <>
       <header className="w-full flex justify-between md:items-center bg-transparent px-16 py-5 border-b-[2px] fixed">
         <div>
-          <p className="tracking-[4px] text-[yellowgreen] text-xl cursor-pointer opacity-60 hover:opacity-100 transition duration-700 delay-100 ease-in-out">
+          <p className="tracking-[4px] text-[yellowgreen] text-xl cursor-pointer bold transition duration-700 delay-100 ease-in-out">
             LOGO
           </p>
         </div>
@@ -18,12 +18,12 @@ const Header = () => {
           <li className="text-lg opacity-60 cursor-pointer hover:opacity-100 text-[yellowgreen] transition delay-100 duration-700 ease-in-out">
             Home
           </li>
-          <li className="text-lg opacity-60 cursor-pointer hover:opacity-100  transition delay-100 duration-700 ease-in-out">
+          <li className="text-lg opacity-60 cursor-pointer hover:opacity-100  transition delay-100 duration-700 ease-in-out text-white">
             Prodcuts
           </li>
            <div  >
           <div>
-             <li className="flex  items-center gap-1 list-none text-lg cursor-pointer hover:text-[yellowgreen]  transition delay-100 duration-700 ease-in-out" onClick={()=>{setServiceToggle(!serviceToggle)}} >
+             <li className="flex  items-center gap-1 list-none text-lg cursor-pointer opacity-50 hover:opacity-100 transition delay-100 duration-700 ease-in-out text-white" onClick={()=>{setServiceToggle(!serviceToggle)}} >
             Service <ArrowDown size={15} />
             </li>
           </div>
@@ -31,9 +31,9 @@ const Header = () => {
         serviceToggle &&(
           
             <ul className="w-44 bg-[#fff] absolute top-[70px] right-20 shadow-xl rounded-sm"> 
-                <li className="px-5  text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Engage</li>
-                <li className="px-5 py-0.5 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Pontificate</li>
-                <li className="px-5 py-0.5 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Synergize</li>
+                <li className="px-5  text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer ">Engage</li>
+                <li className="px-5 py-0.5 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer ">Pontificate</li>
+                <li className="px-5 py-0.5 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer ">Synergize</li>
 
             </ul>
         )
@@ -41,7 +41,7 @@ const Header = () => {
 
           
          </div>
-          <li className="text-lg opacity-60 cursor-pointer hover:opacity-100 transition delay-100 duration-700 ease-in-out">
+          <li className="text-lg opacity-60 cursor-pointer hover:opacity-100 transition delay-100 duration-700 ease-in-out text-white">
             Contact
           </li>
         </ul>
@@ -50,7 +50,7 @@ const Header = () => {
         </div>
       </header>
       {toggle && (
-        <div className="w-full absolute top-20  md:hidden">
+        <div className="w-full absolute top-16  md:hidden bg-white">
           <li className="list-none px-5 py-2 text-lg cursor-pointer text-[yellowgreen] hover:text-[yellowgreen]  transition delay-100 duration-700 ease-in-out">
             Home
           </li>
@@ -67,9 +67,9 @@ const Header = () => {
         serviceToggle &&(
           
             <ul className="w-full"> 
-                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Engage</li>
-                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Pontificate</li>
-                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer">Synergize</li>
+                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer opacity-70">Engage</li>
+                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer opacity-70">Pontificate</li>
+                <li className="px-8 py-1 text-lg hover:text-[yellowgreen] transition duration-700 delay-100 ease-in-out cursor-pointer opacity-70">Synergize</li>
 
             </ul>
         )
--- new1/src/components/Hero.tsx
+++ new1/src/components/Hero.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen' style={{background:"url(../../public/images/header.jpg)"}}>
-        <h1>Dramatically Engage</h1>
+    <div className='w-full h-screen flex justify-center items-center bg-[url("../../public/images/header.jpg")] bg-cover bg-center' >
+        <h1 >Dramatically Engage</h1>
       
     </div>
   )
