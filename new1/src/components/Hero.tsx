3839e0b83ba91e3a064c4013d6f3b18057cdff9f
Basit Ali <opoali911@gmail.com>
2025-06-24 12:52:05 +0500

    now moving to create next component

--- new1/src/components/Hero.tsx
+++ new1/src/components/Hero.tsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import heroImage from '../../public/images/header.jpg'
-import Image from 'next/image'
+import React from "react";
+import heroImage from "../../public/images/header.jpg";
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center gap-3 md:gap-6 lg:gap-10 bg-[url("../../public/images/header.jpg")] bg-cover bg-center' >
-        <h1 className='text-center text-2xl md:text-5xl lg:text-8xl tracking-[3px]  font-bold text-white '>Dramatically Engage</h1>
-        <p className='text-white text-center text-sm md:text-lg lg:text-xl '>Objectively innovate empowered manufactured products whereas parallel platforms.</p>
-        <div className='mt-5'>
-            <button  className='px-4 md:px-10 py-2 md:py-5 border-none outline-none rounded-full bg-[yellowgreen] text-white hover:bg-[#8cbb2e] text-sm md:text-lg lg:text-xl'>ENGAGE NOW</button>
-        </div>
-      
+    <div className='w-full h-screen flex flex-col justify-center items-center gap-3 md:gap-6 lg:gap-10 bg-[url("../../public/images/header.jpg")] bg-cover bg-center'>
+      <h1 className="text-center text-2xl md:text-5xl lg:text-8xl tracking-[3px]  font-bold text-white ">
+        Dramatically Engage
+      </h1>
+      <p className="text-white text-center text-sm md:text-lg lg:text-xl ">
+        Objectively innovate empowered manufactured products whereas parallel
+        platforms.
+      </p>
+      <div className="mt-5">
+        <button className="px-4 md:px-10 py-2 md:py-5 border-none outline-none rounded-full bg-[yellowgreen] text-white hover:bg-[#8cbb2e] text-sm md:text-lg lg:text-xl">
+          ENGAGE NOW
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
