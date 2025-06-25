da7a17c728f34d6f0e98e7d1ffe06cf2b1b9078e
Basit Ali <opoali911@gmail.com>
2025-06-25 10:05:04 +0500

    working in header component

--- new1/src/components/Header.tsx
+++ new1/src/components/Header.tsx
@@ -3,6 +3,24 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
+        <header>
+            <div>
+                <p>LOGO</p>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Prodcuts</li>
+                <li>Service</li>
+                <li>Contactx</li>
+
+            </ul>
+        </header>
+        <main>
+            <h1>Dramatically Engage</h1>
+            <p>Objectively innovate empowered manufactured products whereas parallel platforms.
+
+</p>
+        </main>
       
     </div>
   )
