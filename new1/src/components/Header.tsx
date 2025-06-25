fc0b00e3ca057ed7e9a88df210c8f1a6775fc064
Basit Ali <opoali911@gmail.com>
2025-06-25 10:24:37 +0500

    working in header

--- new1/src/components/Header.tsx
+++ new1/src/components/Header.tsx
@@ -5,10 +5,12 @@ import { MenuSquare } from "lucide-react";
 const Header = () => {
   const [toggle, setToggle] = useState(false);
 
+
   return (
+    <>
     <header className="w-full flex justify-between items-center bg-transparent p-6 border-b-[2px] fixed">
       <div>
-        <p className="tracking-[4px]">LOGO</p>
+        <p className="tracking-[4px] text-[yellowgreen]">LOGO</p>
       </div>
       <ul className="hidden lg:flex gap-12">
         <li>Home</li>
@@ -17,17 +19,19 @@ const Header = () => {
         <li>Contact</li>
       </ul>
       <div className="md:hidden">
-        <MenuSquare />
-        {toggle && (
-          <div>
-            <li>Home</li>
-            <li>Prodcuts</li>
-            <li>Service</li>
-            <li>Contact</li>
-          </div>
-        )}
+        <MenuSquare onClick={()=>setToggle(!toggle)}/>
+       
       </div>
     </header>
+     {toggle && (
+          <div className="w-full bg-red-200 absolute top-14 transition-all">
+            <li className="list-none border-b p-2 text-[yellowgreen]">Home</li>
+            <li className="list-none border-b p-2">Prodcuts</li>
+            <li className="list-none border-b p-2">Service</li>
+            <li className="list-none border-b p-2">Contact</li>
+          </div>
+        )}
+    </>
   );
 };
 
