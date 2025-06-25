d4c57e0f8c9b0b9d346e9343de22217c9929528c
Basit Ali <opoali911@gmail.com>
2025-06-25 09:58:44 +0500

    remove irrelevent codes

--- new1/src/app/page.tsx
+++ new1/src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div>
-      <Button variant={"destructive"} size={"lg"}>Button</Button>
+    <>
+    
       
-    </div>
+    </>
   )
 }
 
