c318c0d8ce38d9299696e8b2e28f87e481e5d613
Basit Ali <opoali911@gmail.com>
2025-06-25 10:17:21 +0500

    now create humbar menu

--- new1/src/components/Header.tsx
+++ new1/src/components/Header.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { MenuSquare } from "lucide-react";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <header className="flex justify-between items-center bg-transparent p-6 border-b-[2px]">
+    <header className="w-full flex justify-between items-center bg-transparent p-6 border-b-[2px] fixed">
       <div>
-        <p className="">LOGO</p>
+        <p className="tracking-[4px]">LOGO</p>
       </div>
-      <ul className="flex gap-10">
+      <ul className="hidden lg:flex gap-12">
         <li>Home</li>
         <li>Prodcuts</li>
         <li>Service</li>
         <li>Contact</li>
       </ul>
+      <div className="md:hidden">
+        <MenuSquare />
+        {toggle && (
+          <div>
+            <li>Home</li>
+            <li>Prodcuts</li>
+            <li>Service</li>
+            <li>Contact</li>
+          </div>
+        )}
+      </div>
     </header>
   );
 };
