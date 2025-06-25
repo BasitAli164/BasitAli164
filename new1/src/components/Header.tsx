a2b4d9ad58e6fa07a7eef164a380ee2e5d095a26
Basit Ali <opoali911@gmail.com>
2025-06-25 10:05:42 +0500

    structure of header is almost ready

--- new1/src/components/Header.tsx
+++ new1/src/components/Header.tsx
@@ -1,29 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-        <header>
-            <div>
-                <p>LOGO</p>
-            </div>
-            <ul>
-                <li>Home</li>
-                <li>Prodcuts</li>
-                <li>Service</li>
-                <li>Contactx</li>
+    <header>
+      <div>
+        <p>LOGO</p>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Prodcuts</li>
+        <li>Service</li>
+        <li>Contactx</li>
+      </ul>
+    </header>
+  );
+};
 
-            </ul>
-        </header>
-        <main>
-            <h1>Dramatically Engage</h1>
-            <p>Objectively innovate empowered manufactured products whereas parallel platforms.
-
-</p>
-        </main>
-      
-    </div>
-  )
-}
-
-export default Header
+export default Header;
