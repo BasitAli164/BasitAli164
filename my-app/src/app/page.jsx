120e5938615d176388776820c95889496a41d069
Basit Ali <opoali911@gmail.com>
2025-07-03 13:49:28 +0500

    now start to work in header

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,9 +1,11 @@
 import MiniHeader from './component/navbar/aboveHeader'
+import Header from './component/navbar/mainHeader'
 
 const HomePage = () => {
   return (
   <>
   <MiniHeader/>
+  <Header/>
   </>
   )
 }
