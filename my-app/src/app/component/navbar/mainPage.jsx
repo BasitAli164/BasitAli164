db2b252d5ce70f261e6c864638e2874d82eabcc4
Basit Ali <opoali911@gmail.com>
2025-07-03 14:54:55 +0500

    craete main page also

---
+++ my-app/src/app/component/navbar/mainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='mainContainerOfMainPage'>
+        <h1>Welcome to YourCorp</h1>
+        <p>This page content demonstrates how the sticky header stays at the top of the viewport when you scroll.</p>
+      
+    </div>
+  )
+}
+
+export default Main
--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,11 +1,13 @@
 import MiniHeader from './component/navbar/aboveHeader'
 import Header from './component/navbar/mainHeader'
+import Main from './component/navbar/mainPage'
 
 const HomePage = () => {
   return (
   <>
   <MiniHeader/>
   <Header/>
+  <Main/>
   </>
   )
 }
