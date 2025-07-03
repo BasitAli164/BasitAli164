6551202e2d76a5404d0a9644a0ee59f0486052b2
Basit Ali <opoali911@gmail.com>
2025-07-03 14:39:41 +0500

    fix the bugs

--- my-app/src/app/component/navbar/mainHeader.jsx
+++ my-app/src/app/component/navbar/mainHeader.jsx
@@ -1,4 +1,4 @@
-import { Pyramid, Triangle } from 'lucide-react'
+import { ArrowDown, Pyramid, Triangle } from 'lucide-react'
 import React from 'react'
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
         </div>
         <div className="menulist">
             <button className='mainbtns'>Home</button>
-            <button className='mainbtns '>Services <Triangle className='triangle'/> </button>
+            <button className='mainbtns specialbtn '>Services <ArrowDown  className='arrow'/> </button>
             <button className='mainbtns'>About Us</button>
             <button className='mainbtns'>Blog</button>
             <button className='mainheaderbtn'>Contact Us</button>
