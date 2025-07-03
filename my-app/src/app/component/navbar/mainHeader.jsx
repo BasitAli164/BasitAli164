fa1c2c47dbe5d3b6d03a7d85cea7e78119acacb9
Basit Ali <opoali911@gmail.com>
2025-07-03 14:06:39 +0500

    working in main header style

--- my-app/src/app/component/navbar/mainHeader.jsx
+++ my-app/src/app/component/navbar/mainHeader.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='mainContainer'>
+        <div className="logo">
+            <h1>YourCrop</h1>
+        </div>
+        <div className="menulist">
+            <button>Home</button>
+            <button>Services</button>
+            <button>About Us</button>
+            <button>Blog</button>
+            <button>Contact Us</button>
+        </div>
       
     </div>
   )
