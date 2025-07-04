f8a8c6fcbc7549c39574b43c2ac95b1c9a7226b3
Basit Ali <opoali911@gmail.com>
2025-07-04 13:35:03 +0500

    almost complete the upper header component

--- my-app/src/app/component/navbar/aboveHeader.jsx
+++ my-app/src/app/component/navbar/aboveHeader.jsx
@@ -1,20 +1,24 @@
-import { Mail, Phone } from "lucide-react";
+import { Linkedin, Mail, Phone, TwitterIcon } from "lucide-react";
 
 const MiniHeader = () => {
   return (
     <div className="miniContainer">
       <div className="contactDetail">
         <div className="number">
-          <Phone width={14} />
-          <p>+923 475495500</p>
+          <Phone />
+          <p className="minipara">+923 475495500</p>
         </div>
         <div className="mail">
-            <Mail width={14}/>
-            <p>basitdeveloper911@gmail.com</p>
+            <Mail/>
+            <p className="minipara">basitdeveloper911@gmail.com</p>
         </div>
 
+
       </div>
-      <div className="socail"></div>
+      <div className="socail">
+        <Linkedin/>
+        <TwitterIcon/>
+      </div>
     </div>
   );
 };
--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="Mountain Travels  Pakistan" content="This is the best site to reach mountain travelers"/>
       </head>
-      <body>
+      <body className='main'>
         
         {children}
        
