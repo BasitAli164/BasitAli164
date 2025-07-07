36011e71c40d2a906d81319eb9f90a9ec902ce06
Basit Ali <opoali911@gmail.com>
2025-07-07 13:22:33 +0500

    working in email template

--- my-app/src/app/component/template.jsx
+++ my-app/src/app/component/template.jsx
@@ -1,40 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const EmailTemplate = () => {
   return (
-   <div className=' flex justify-center items-center bg-[#1b1e1f]'>
-     <div className='w-1/2  bg-gray-900 text-[#e9e9e9] border-[7px] border-solid border-red-800 '>
-        <div className='p-20  text-center'>
-            <h1 className='text-4xl font-bold text-[#3092ea] '>Order Confirmed!</h1>
-            <p className='text-md mt-3'>Thanks for your order, [Customer Name]</p>
+    <div className=" flex justify-center items-center bg-[#1b1e1f]">
+      <div className="w-1/2  bg-gray-900 text-[#e9e9e9] border-[7px] border-solid border-red-800 ">
+        <div className="p-20  text-center">
+          <h1 className="text-4xl font-bold text-[#3092ea] ">
+            Order Confirmed!
+          </h1>
+          <p className="text-md mt-3">Thanks for your order, [Customer Name]</p>
         </div>
-        <div className='bg-[#1c1f20] p-10 flex flex-col gap-10'>
+        <div className="bg-[#1c1f20] p-10 flex flex-col gap-10">
+          <div>
+            <h3 className="text-3xl font-semibold">Order #[Order Number]</h3>
+            <p className="text-xl mt-2">Placed on [Order Date]</p>
+          </div>
+          <strong className="font-bold text-3xl text-[#3092ea]">
+            Total: $[Total Amount]
+          </strong>
+        </div>
+        <div className="p-10 w-full">
+          <p className="text-[#3092ea] font-bold text-xl">Your Item</p>
+          <div className="p-10 flex flex-col gap-14">
+            <div className="flex justify-between ">
+              <div>
+                <p className="font-bold text-2xl">[Product Name 1]</p>
+                <p className="text-xl">Qty: 1 • [Product Code]</p>
+              </div>
+              <div>
+                <p className="text-2xl font-bold">$[Price]</p>
+              </div>
+            </div>
+            <div className="flex justify-between ">
+              <div>
+                <p className="font-bold text-2xl">[Product Name 2]</p>
+                <p className="text-xl">Qty: 1 • [Product Code]</p>
+              </div>
+              <div>
+                <p className="text-2xl font-bold">$[Price]</p>
+              </div>
+            </div>
             <div>
-                <h3 className='text-3xl font-semibold'>Order #[Order Number]</h3>
-                <p className='text-xl mt-2'>Placed on [Order Date]</p>
+              <div>
+                <p>Subtotal:</p>
+                <p>Shipping:</p>
+                <p>Tax:</p>
+              </div>
+              <div>
+                <p>$[Subtotal]</p>
+                <p>$[Shipping]</p>
+                <p> $[Tax]</p>
+              </div>
             </div>
-            <strong className='font-bold text-3xl text-[#3092ea]'>Total: $[Total Amount]</strong>
-
+          </div>
         </div>
-        <div className='p-10 w-full'>
-            <p className='text-[#3092ea] font-bold text-xl'>Your Item</p>
-            <div className='p-10'>
-                <div className='flex justify-between border-[5px] border-solid border-white '>
-                    <div >
-                        <p className='font-bold text-2xl'>[Product Name 1]</p>
-                        <p className='text-xl'>Qty: 1 • [Product Code]</p>
-                    </div>
-                    <div>
-                        <p className='text-2xl font-bold'>$[Price]</p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-      
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default EmailTemplate
+export default EmailTemplate;
