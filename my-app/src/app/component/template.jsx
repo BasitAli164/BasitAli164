dbf7c1e6f48aeb62150a73f9043705928dde3b1c
Basit Ali <opoali911@gmail.com>
2025-07-08 12:17:32 +0500

    complete the header and hero section of the order email template

--- my-app/src/app/component/template.jsx
+++ my-app/src/app/component/template.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const EmailTemplate = () => {
   return (
    <div className=' flex justify-center items-center bg-[#1b1e1f]'>
-     <div className='w-1/2  bg-gray-900 text-[#e9e9e9] border-[7px] border-solid border-red-800 shadow-xl'>
+     <div className='w-1/2  bg-gray-900 text-[#e9e9e9] border-[7px] border-solid border-red-800 '>
         <div className='p-20  text-center'>
             <h1 className='text-4xl font-bold text-[#3092ea] '>Order Confirmed!</h1>
             <p className='text-md mt-3'>Thanks for your order, [Customer Name]</p>
@@ -15,6 +15,9 @@ const EmailTemplate = () => {
             </div>
             <strong className='font-bold text-3xl text-[#3092ea]'>Total: $[Total Amount]</strong>
 
+        </div>
+        <div className='p-10'>
+            <p className='text-[#3092ea] font-bold text-xl'>Your Item</p>
         </div>
       
     </div>
