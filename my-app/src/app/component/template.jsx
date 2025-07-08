4494138611c09e1e374d7bc4f290a0e23aaaa630
Basit Ali <opoali911@gmail.com>
2025-07-08 12:15:06 +0500

    working in email template

--- my-app/src/app/component/template.jsx
+++ my-app/src/app/component/template.jsx
@@ -2,20 +2,23 @@ import React from 'react'
 
 const EmailTemplate = () => {
   return (
-    <div className='h-screen w-full'>
-        <div className='p-24 bg-blue-400 text-center'>
-            <h1 className='text-5xl font-bold '>Order Confirmed!</h1>
-            <p className='text-xl mt-3'>Thanks for your order, [Customer Name]</p>
+   <div className=' flex justify-center items-center bg-[#1b1e1f]'>
+     <div className='w-1/2  bg-gray-900 text-[#e9e9e9] border-[7px] border-solid border-red-800 shadow-xl'>
+        <div className='p-20  text-center'>
+            <h1 className='text-4xl font-bold text-[#3092ea] '>Order Confirmed!</h1>
+            <p className='text-md mt-3'>Thanks for your order, [Customer Name]</p>
         </div>
-        <div>
+        <div className='bg-[#1c1f20] p-10 flex flex-col gap-10'>
             <div>
-                <h3>Order #[Order Number]</h3>
-                <p>Placed on [Order Date]</p>
+                <h3 className='text-3xl font-semibold'>Order #[Order Number]</h3>
+                <p className='text-xl mt-2'>Placed on [Order Date]</p>
             </div>
+            <strong className='font-bold text-3xl text-[#3092ea]'>Total: $[Total Amount]</strong>
 
         </div>
       
     </div>
+   </div>
   )
 }
 
