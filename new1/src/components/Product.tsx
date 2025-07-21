7aeb01ba5427f158eecaffee0288b332e8632863
Basit Ali <opoali911@gmail.com>
2025-07-21 19:15:26 +0500

    above section is complete

--- new1/src/components/Product.tsx
+++ new1/src/components/Product.tsx
@@ -4,7 +4,7 @@ import img2 from '../../public/images/iphone.jpg'
 const Product = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-10 sm:gap-12 md:gap-14 lg:gap-16 xl:gap-20 xl:p-32'>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-10 sm:gap-12 md:gap-14 lg:gap-16 xl:gap-20 p-8 sm:p-10 md:p-14 '>
             <div className='border-[3px] border-red-500'>
               <Image src={img1} alt='microphone-img' className='rounded-full'/>
             </div>
@@ -13,7 +13,7 @@ const Product = () => {
               <h4 className='text-lg sm:text-xl md:text-2xl xl:text-3xl font-semibold tracking-[1px]'>Best in Class</h4>
               <p className='text-sm md:text-lg lg:text-xl xl:text-2xl opacity-70'>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
               <div >
-                <button className='px-12 py-5 bg-[yellowgreen] border-none outline-none rounded-full text-white text-xl '>CLASSIFY IT</button>
+                <button className='px-6 lg:px-10 py-3 md:py-5 bg-[yellowgreen] border-none outline-none rounded-full text-white text-sm lg:text-lg  '>CLASSIFY IT</button>
               </div>
             </div>
            
