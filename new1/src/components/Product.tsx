0fe091aaa8519011313d0ecbdc5ea509a82c9ae0
Basit Ali <opoali911@gmail.com>
2025-07-21 19:10:49 +0500

    working in mobile responsiveness

--- new1/src/components/Product.tsx
+++ new1/src/components/Product.tsx
@@ -4,15 +4,16 @@ import img2 from '../../public/images/iphone.jpg'
 const Product = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <div className='flex justify-center items-center gap-20 p-32'>
-            <div className='w-10/12'>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-10 sm:gap-12 md:gap-14 lg:gap-16 xl:gap-20 xl:p-32'>
+            <div className='border-[3px] border-red-500'>
               <Image src={img1} alt='microphone-img' className='rounded-full'/>
             </div>
-            <div className='flex flex-col justify-center items-center gap-32'>
-              <h4>Best in Class</h4>
-              <p>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-              <div>
-                <button>CLASSIFY IT</button>
+            
+            <div className='flex flex-col justify-center items-start gap-2 sm:gap-4 md:gap-6 lg:gap-8'>
+              <h4 className='text-lg sm:text-xl md:text-2xl xl:text-3xl font-semibold tracking-[1px]'>Best in Class</h4>
+              <p className='text-sm md:text-lg lg:text-xl xl:text-2xl opacity-70'>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+              <div >
+                <button className='px-12 py-5 bg-[yellowgreen] border-none outline-none rounded-full text-white text-xl '>CLASSIFY IT</button>
               </div>
             </div>
            
