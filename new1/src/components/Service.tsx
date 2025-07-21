4d534d4ed5cc0405ca80b6f768ffab2704bc4427
Basit Ali <opoali911@gmail.com>
2025-07-21 20:13:46 +0500

    almost complete the service page

--- new1/src/components/Service.tsx
+++ new1/src/components/Service.tsx
@@ -1,15 +1,21 @@
 
 const Service = () => {
   return (
-    <div>
-        <div className="bg-[url(/images/concert.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center">
+    <div className="w-full h-screen">
+        <div className="bg-[url(/images/concert.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+
         <h3></h3>
+        
         </div>
-        <div className="bg-[url(/images/pencil_sharpener.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center">
+        <div className="bg-[url(/images/pencil_sharpener.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+
         <h3></h3>
+        
         </div>
-        <div className="bg-[url(/images/writing.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center">
+        <div className="bg-[url(/images/writing.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+
         <h3></h3>
+        
         </div>
       
     </div>
