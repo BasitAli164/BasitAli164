7dbdff46670cf1b073023d3b4f98a2043217aab3
Basit Ali <opoali911@gmail.com>
2025-07-21 18:56:42 +0500

    now make the product component

--- new1/src/components/Product.tsx
+++ new1/src/components/Product.tsx
@@ -3,10 +3,10 @@ import img1 from '../../public/images/microphone.jpg'
 import img2 from '../../public/images/iphone.jpg'
 const Product = () => {
   return (
-    <div className='flex'>
-        <div>
-            <div>
-              <Image src={img1} alt='microphone-img'/>
+    <div className='flex flex-col justify-center items-center'>
+        <div className='flex justify-center items-center gap-20 p-32'>
+            <div className='w-10/12'>
+              <Image src={img1} alt='microphone-img' className='rounded-full'/>
             </div>
             <div>
               <h4>Best in Class</h4>
@@ -15,7 +15,10 @@ const Product = () => {
                 <button>CLASSIFY IT</button>
               </div>
             </div>
-            <div>
+           
+        </div>
+        <div >
+           <div className='flex'>
               <div>
                 <h4>Superior Quality</h4>
                 <p>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
@@ -28,8 +31,8 @@ const Product = () => {
                 <Image src={img2} alt='ipone-img'/>
               </div>
             </div>
+
         </div>
-        <div></div>
       
     </div>
   )
