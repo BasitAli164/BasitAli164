827a339a1b0f99ff2936ae46f6fd8f37c35317c4
Basit Ali <opoali911@gmail.com>
2025-07-21 20:34:47 +0500

    fix the issue

--- new1/src/components/Service.tsx
+++ new1/src/components/Service.tsx
@@ -1,20 +1,20 @@
 
 const Service = () => {
   return (
-    <div className="w-full h-screen">
-        <div className="bg-[url(/images/concert.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+    <div className=" w-full  flex flex-col xl:flex-row justify-center  ">
+        <div className="bg-[url(/images/concert.jpg)] bg-cover bg-center flex justify-center items-center transform hover:scale-[1.08] cursor-pointer transition duration-700 delay-100  w-full h-64">
 
-        <h3></h3>
+        <h3 className="text-5xl xl:text-2xl text-white">Synergize</h3>
         
         </div>
-        <div className="bg-[url(/images/pencil_sharpener.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+        <div className="bg-[url(/images/pencil_sharpener.jpg)] bg-cover bg-center flex justify-center items-center transform hover:scale-[1.09] cursor-pointer transition duration-700 delay-100 w-full h-64">
 
-        <h3></h3>
+        <h3 className="text-5xl xl:text-2xl text-white">Pontificate</h3>
         
         </div>
-        <div className="bg-[url(/images/writing.jpg)] bg-cover bg-center w-full h-72 flex justify-center items-center transform hover:scale-110 cursor-pointer">
+        <div className="bg-[url(/images/writing.jpg)] bg-cover bg-center  flex justify-center items-center transform hover:scale-[1.09] cursor-pointer transition duration-700 delay-100 w-full h-64">
 
-        <h3></h3>
+        <h3 className="text-5xl xl:text-2xl text-white">Engage</h3>
         
         </div>
       
