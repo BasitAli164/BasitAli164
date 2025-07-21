c1c844a30f7b871da671e6368b67188a65624df4
Basit Ali <opoali911@gmail.com>
2025-07-21 19:16:18 +0500

    now start to work in another section

--- new1/src/components/Product.tsx
+++ new1/src/components/Product.tsx
@@ -1,42 +1,51 @@
-import Image from 'next/image'
-import img1 from '../../public/images/microphone.jpg'
-import img2 from '../../public/images/iphone.jpg'
+import Image from "next/image";
+import img1 from "../../public/images/microphone.jpg";
+import img2 from "../../public/images/iphone.jpg";
 const Product = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col md:flex-row justify-center items-center gap-10 sm:gap-12 md:gap-14 lg:gap-16 xl:gap-20 p-8 sm:p-10 md:p-14 '>
-            <div className='border-[3px] border-red-500'>
-              <Image src={img1} alt='microphone-img' className='rounded-full'/>
-            </div>
-            
-            <div className='flex flex-col justify-center items-start gap-2 sm:gap-4 md:gap-6 lg:gap-8'>
-              <h4 className='text-lg sm:text-xl md:text-2xl xl:text-3xl font-semibold tracking-[1px]'>Best in Class</h4>
-              <p className='text-sm md:text-lg lg:text-xl xl:text-2xl opacity-70'>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-              <div >
-                <button className='px-6 lg:px-10 py-3 md:py-5 bg-[yellowgreen] border-none outline-none rounded-full text-white text-sm lg:text-lg  '>CLASSIFY IT</button>
-              </div>
-            </div>
-           
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-10 sm:gap-12 md:gap-14 lg:gap-16 xl:gap-20 p-8 sm:p-10 md:p-14 ">
+        <div className="border-[3px] border-red-500">
+          <Image src={img1} alt="microphone-img" className="rounded-full" />
         </div>
-        <div >
-           <div className='flex'>
-              <div>
-                <h4>Superior Quality</h4>
-                <p>Holisticly predominate extensible testing procedures for reliable supply chains. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-                <div>
-                  <button>TEST IT</button>
-                </div>
-              </div>
 
-              <div>
-                <Image src={img2} alt='ipone-img'/>
-              </div>
-            </div>
-
+        <div className="flex flex-col justify-center items-start gap-2 sm:gap-4 md:gap-6 lg:gap-8">
+          <h4 className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-semibold tracking-[1px]">
+            Best in Class
+          </h4>
+          <p className="text-sm md:text-lg lg:text-xl xl:text-2xl opacity-70">
+            Holisticly predominate extensible testing procedures for reliable
+            supply chains. Dynamically innovate resource-leveling customer
+            service for state of the art customer service.
+          </p>
+          <div>
+            <button className="px-6 lg:px-10 py-3 md:py-5 bg-[yellowgreen] border-none outline-none rounded-full text-white text-sm lg:text-lg  ">
+              CLASSIFY IT
+            </button>
+          </div>
         </div>
-      
+      </div>
+      <div>
+        <div className="flex">
+          <div>
+            <h4>Superior Quality</h4>
+            <p>
+              Holisticly predominate extensible testing procedures for reliable
+              supply chains. Dynamically innovate resource-leveling customer
+              service for state of the art customer service.
+            </p>
+            <div>
+              <button>TEST IT</button>
+            </div>
+          </div>
+
+          <div>
+            <Image src={img2} alt="ipone-img" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
