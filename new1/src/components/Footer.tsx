98c06e09071f2feddb856953d3c18a17a2b59d64
Basit Ali <opoali911@gmail.com>
2025-07-22 08:52:44 +0500

    complete the project

--- new1/src/components/Footer.tsx
+++ new1/src/components/Footer.tsx
@@ -1,10 +1,9 @@
 const Footer = () => {
   return (
     <div className="w-full p-10">
-        <p className="text-xl text-center">Copyright © Example.com 2015</p>
-      
+      <p className="text-xl text-center">Copyright © Example.com 2015</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
