abfddebdaf8038791ea991d904b6fbc1dbc46620
Basit Ali <opoali911@gmail.com>
2025-07-23 15:04:07 +0500

    explore dynamic routing

--- without/src/app/about/services/[id]/page.tsx
+++ without/src/app/about/services/[id]/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const  page = async({params}) => {
+  const parameter=await params
+  console.log(parameter)
   return (
     <div>
+      <h1>Serverice </h1>
       
     </div>
   )
--- without/src/app/about/services/page.tsx
+++ without/src/app/about/services/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Service = () => {
   return (
-    <div>
-        <ul>
+    <div className='w-full h-screen mt-10 text-center  '>
+        <ul >
             <li>Web Development</li>
             <li>Mobile App Development`</li>
             <li>AI Products</li>
