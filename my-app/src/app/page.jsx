5e9d17b7c7c1b889c9d8d9c196374df4feb66670
Basit Ali <opoali911@gmail.com>
2025-07-23 13:47:25 +0500

    again setup the tailwind css

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,13 +1,16 @@
+"use client"
+import { useState } from "react"
 import Footer from "./component/Footer"
 import Header from "./component/Header"
 
 
 const HomePage = () => {
-  
+  const [count,setCount]=useState(1)
+
   return (
   <>
  <Header/>
- <h1>Welcome to Home Page</h1>
+ <h1>Welcome to Home Page {count}</h1>
  <Footer/>
   </>
   )
