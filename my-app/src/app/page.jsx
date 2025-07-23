b292cefd3f53dc55273773717cbf6768040e46f0
Basit Ali <opoali911@gmail.com>
2025-07-23 13:43:44 +0500

    START TO EXPLORE CONTEXT API

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,13 +1,9 @@
-import MiniHeader from './component/navbar/aboveHeader'
-import Header from './component/navbar/mainHeader'
-import Main from './component/navbar/mainPage'
+
 
 const HomePage = () => {
   return (
   <>
-  <MiniHeader/>
-  <Header/>
-  <Main/>
+ 
   </>
   )
 }
