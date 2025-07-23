654997bad1a09d693ed684099f1a920bd4cecd98
Basit Ali <opoali911@gmail.com>
2025-07-23 13:45:43 +0500

    import both component in home page

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,9 +1,14 @@
+import Footer from "./component/Footer"
+import Header from "./component/Header"
 
 
 const HomePage = () => {
+  
   return (
   <>
- 
+ <Header/>
+ <h1>Welcome to Home Page</h1>
+ <Footer/>
   </>
   )
 }
