0dbf237912fdcdb942ad3ca5a408fbec3708b92e
Basit Ali <opoali911@gmail.com>
2025-07-24 14:31:09 +0500

    create route buy using link component of next js

--- without/src/app/about/page.tsx
+++ without/src/app/about/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const About = () => {
   return (
-    <div className='mt-10 text-center'>
+    <div className='w-full h-screen flex justify-center items-center gap-10'>
         <h1 className='text-4xl text-green-800 font-bold'>About Page</h1>
+        <div className='    '>
+            <Link className='px-10 py-4 rounded-lg bg-red-600 font-bold text-xl text-white' href={"/"}>Home</Link>
+        </div>
       
     </div>
   )
