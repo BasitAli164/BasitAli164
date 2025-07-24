fa79d82589a9455aa29ff5640354971e9995a258
Basit Ali <opoali911@gmail.com>
2025-07-24 12:28:38 +0500

    fully update the file and folder structure

--- new1/src/components/Template-1/Template.tsx
+++ new1/src/components/Template-1/Template.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Advertise from './advertise'
+import Footer from './Footer'
+import Header from './Header'
+import Hero from './Hero'
+import Product from './Product'
+import Contact from './Contact'
+import Service from './Service'
 
 const Template = () => {
   return (
     <div>
+        <Header/>
+        <Hero/>
+        <Advertise/>
+        <Product/>
+        <Service/>
+        <Contact/>
+        <Footer/>
       
     </div>
   )
