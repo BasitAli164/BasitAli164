7641257e5350cd9c02d8f5479e041e05bfc020d6
Basit Ali <opoali911@gmail.com>
2025-07-24 12:26:54 +0500

    update file and folder structure

--- new1/src/app/page.tsx
+++ new1/src/app/page.tsx
@@ -1,10 +1,4 @@
-import Advertise from "@/components/advertise"
-import Header from "@/components/Header"
-import Hero from "@/components/Hero"
-import Product from "@/components/Product"
-import Service from "@/components/Service"
-import Contact from '@/components/Contact'
-import Footer from "@/components/Footer"
+
 
 const Home = () => {
   return (
