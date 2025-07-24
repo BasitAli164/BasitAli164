c043670b7029d64505076ef284a4d1aa98e623e2
Basit Ali <opoali911@gmail.com>
2025-07-24 12:32:56 +0500

    now start to create a the components of the second template

--- new1/src/app/page.tsx
+++ new1/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Template from "@/components/Template-1/Template"
+import Template from "@/components/Template-1/Template1"
+import Template2 from "@/components/Template-2/Template2"
 
 
 const Home = () => {
   return (
     <>
     {/* <Template/> */}
+    <Template2/>
    
       
     </>
--- new1/src/components/Template-2/Template2.tsx
+++ new1/src/components/Template-2/Template2.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Template2 = () => {
   return (
     <div>
-      
+      <h1>Try to something new</h1>
     </div>
   )
 }
