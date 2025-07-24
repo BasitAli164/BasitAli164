ac03d0f3a66922736533946b09613315bac5692a
Basit Ali <opoali911@gmail.com>
2025-07-24 14:27:05 +0500

    explore the default not found page in next js

--- without/src/app/page.tsx
+++ without/src/app/page.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='w-full h-screen flex gap-10 items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Home Page</h1>
+      <div>
+      <Link className='px-8 py-4 rounded-lg text-xl bg-red-300' href={'/about'}>About Page</Link>
+
+      </div>
     </div>
   )
 }
