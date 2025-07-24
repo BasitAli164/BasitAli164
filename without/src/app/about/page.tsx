2c578bdc7eb690595e548b981a2d98d8756d5f9e
Basit Ali <opoali911@gmail.com>
2025-07-24 14:28:34 +0500

    learn about how to createe route in next js

---
+++ without/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mt-10 text-center'>
+        <h1 className='text-4xl text-green-800 font-bold'>About Page</h1>
+      
+    </div>
+  )
+}
+
+export default About
