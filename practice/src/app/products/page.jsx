91573c49424863c4cf24a1e0931b8dd59dbda0c0
Basit Ali <opoali911@gmail.com>
2025-07-25 10:50:31 +0500

    creating new product page

--- practice/src/app/products/page.jsx
+++ practice/src/app/products/page.jsx
@@ -1,13 +1,25 @@
+"use client";
+import { useState } from "react";
 
-export const metadata={
-    title:"Products"
-}
+// export const metadata={
+//     title:"Products"
+// }
+
+const data = [
+  { title: "Cake", price: 200, btn: "Order Now" },
+  { title: "Papaar", price: 100, btn: "Order Now" },
+  { title: "Choclate", price: 500, btn: "Order Now" },
+];
 const Product = () => {
+  const [product, setProduct] = useState(data);
+  console.log(product);
   return (
-    <div>
-      
+    <div className="w-full h-screen p-10">
+      {product.map((item, index) => {
+        <div id={index} className="border-2 border-white border-solid"></div>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
