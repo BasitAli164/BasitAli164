253a0a4aad6cb789e19e0b4248cd0cb5b2a3d5bb
Basit Ali <opoali911@gmail.com>
2025-07-25 10:16:49 +0500

    now working on an ecommerce product to understand the routing in next js

--- practice/src/app/page.jsx
+++ practice/src/app/page.jsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      
+    <div className="w-full h-screen flex flex-col justify-center items-center gap-10">
+      <h1 className="text-4xl font-bold">Welcom To Our Shop</h1>
+      <div >
+        <Link className="px-10 py-5 rounded-lg bg-amber-400 font-bold text-xl mx-5 cursor-pointer hover:bg-amber-700 transition duration-700 delay-100 " href={"/about"}>About Us</Link>
+        <Link className="px-10 py-5 rounded-lg bg-amber-400 font-bold text-xl mx-5 cursor-pointer hover:bg-amber-700 transition duration-700 delay-100 " href={'/service'}>Products</Link>
+      </div>
+
       
     </div>
   )
