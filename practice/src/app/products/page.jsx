f65a89f1aa5cf763d2316a450aaf25b78dde6c3b
Basit Ali <opoali911@gmail.com>
2025-07-25 11:09:48 +0500

    facing issue

--- practice/src/app/products/page.jsx
+++ practice/src/app/products/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useRouter } from "next/router";
 import { useState } from "react";
  
 
@@ -11,19 +12,24 @@ const data = [
   { title: "Papaar", price: 100, btn: "Order Now" },
   { title: "Choclate", price: 500, btn: "Order Now" },
 ];
-const Product = () => {
-  const [product, setProduct] = useState(data);
-  console.log(product);
+const Product = ({params}) => {
+    const router=useRouter()
+
+    const orderNow=async()=>{
+        const id=await params;
+        router.push(`product/${id}`)
+    }
+  
   return (
     <div className="w-full h-screen p-10 flex justify-center items-center gap-5 ">
-      {product.map((item, index) => (
+      {data.map((item, index) => (
         <div id={index} className="border-2 border-white border-solid w-72 h-80 p-5  flex flex-col justify-center items-center gap-10 rounded-lg  bg-red-500 ">
             <div>
                 <p className="">Product Name:{item.title}</p>
             <p>Product Price:{item.price}</p>
             </div>
             <div>
-                <button className="px-8 py-4 bg-amber-600 font-bold rounded-xl cursor-pointer">{item.btn}</button>
+                <button className="px-8 py-4 bg-amber-600 font-bold rounded-xl cursor-pointer" onClick={orderNow}>{item.btn}</button>
             </div>
         </div>
       ))}
