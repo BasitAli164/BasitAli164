c610988b4cfba64f3decf709e6687a73117f2094
Basit Ali <opoali911@gmail.com>
2025-07-25 11:02:58 +0500

    working in routing of next js

--- practice/src/app/products/page.jsx
+++ practice/src/app/products/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+ 
 
 // export const metadata={
 //     title:"Products"
@@ -14,10 +15,18 @@ const Product = () => {
   const [product, setProduct] = useState(data);
   console.log(product);
   return (
-    <div className="w-full h-screen p-10">
-      {product.map((item, index) => {
-        <div id={index} className="border-2 border-white border-solid"></div>;
-      })}
+    <div className="w-full h-screen p-10 flex justify-center items-center gap-5 ">
+      {product.map((item, index) => (
+        <div id={index} className="border-2 border-white border-solid w-72 h-80 p-5  flex flex-col justify-center items-center gap-10 rounded-lg  bg-red-500 ">
+            <div>
+                <p className="">Product Name:{item.title}</p>
+            <p>Product Price:{item.price}</p>
+            </div>
+            <div>
+                <button className="px-8 py-4 bg-amber-600 font-bold rounded-xl cursor-pointer">{item.btn}</button>
+            </div>
+        </div>
+      ))}
     </div>
   );
 };
