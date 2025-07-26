4188fe5ad51f6d2fa478405052d29da276c34777
Basit Ali <opoali911@gmail.com>
2025-07-26 09:28:49 +0500

    try to doing work with db

--- practice/src/app/login/page.jsx
+++ practice/src/app/login/page.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+"use client"
+import { useEffect, useState } from 'react'
 
-export default function Login() {
+export default async function Login() {
+    const [userName,setUserName]=useState("")
+    const [email,setEmail]=useState("")
+
+    useEffect(async ()=>{
+        try {
+            const res=await fetch("/api/login")
+            console.log(res.data)
+           
+        } catch (error) {
+            
+        }
+
+    },[])
+
   return (
     <div>
       
