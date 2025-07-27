f8042381a300c9dca2f20b081947d807abada924
Basit Ali <opoali911@gmail.com>
2025-07-27 22:42:18 +0500

    exploring  how to fetch data in client component

---
+++ practice/src/app/post/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import {useEffect, useState} from 'react'
+export default function page() {
+    const [post,setPost]=useState([])
+
+    useEffect(()=>{
+       const fetchData=async ()=>{
+        try {
+            const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+            const data= await res.json()
+            if(!res){
+                return <h1>Does not Fetch</h1>
+            }
+            console.log("Res:",data)
+            setPost(res)
+            
+        } catch (error) {
+            console.log("Error is :",error)
+            
+        }
+       }
+       fetchData()
+    },[])
+
+  return (
+    <div>
+       {
+        post.map((item)=>(
+            <div id={item.id}>
+                <h1>Title of Post: {item.title}</h1>
+
+            </div>
+        ))
+       }
+      
+    </div>
+  )
+}
