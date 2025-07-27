0ea72569d4df3f069e822552bcb9c108ec7f095c
Basit Ali <opoali911@gmail.com>
2025-07-27 19:39:47 +0500

    explore new concept in next js

--- practice/src/app/albums/page.jsx
+++ practice/src/app/albums/page.jsx
@@ -1,6 +1,18 @@
 export default async function Albums() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
-  const data = response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums?_limit=50");
+  const data =await response.json();
+  console.log("Data is: ",data)
 
-  return <div></div>;
+  return (
+    <>
+    <div>
+      {
+        data.map(({id,title})=>(
+          <h1 key={id}>Title is: {title}</h1>
+        ))
+
+      }
+    </div>
+    </>
+  )
 }
