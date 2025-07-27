265050c143305f193f3d3bef05f10c0e93ad7e9c
Basit Ali <opoali911@gmail.com>
2025-07-27 23:21:44 +0500

    exploring new concepts

--- practice/src/app/comments/page.jsx
+++ practice/src/app/comments/page.jsx
@@ -1,44 +1,37 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function page() {
-    const [comment,setComment]=useState([])
-    const [loading,setLoading]=useState(false)
+  const [comment, setComment] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-        const fetchComments=async()=>{
-            try {
-                const response= await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10")
-                const data=await response.json()
-                setLoading(true)
-                setComment(data)
-                console.log(data)
-                
-            } catch (error) {
-                console.log("Error is comment page:",error.message)
-                
-            }
+  useEffect(() => {
+    const fetchComments = async () => {
+      setLoading(true);
 
-        }
-                fetchComments()
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/comment?_limit=10");
+        const data = await response.json();
+        setComment(data);
+        console.log(data);
+      } catch (error) {
+        console.log("Error is comment page:", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchComments();
+  }, []);
 
-        return ()=>{
-            setLoading(false)
-        }
-    },[])
-
-    if(!comment) return <div>There is no Comment</div>
-    if(loading) return <h1 className='text-9xl text-white'>Loading....</h1>
+  if (loading) return <h1 className="text-9xl text-white">Loading....</h1>;
+  if(!comment) return <h2>There is no data fetch</h2>
   return (
     <div>
-        {
-            comment.map((item,index)=>(
-                <div key={item.id}>
-                    <h1>{item.email}</h1>
-                </div>
-            ))
-        }
-      
+      {comment.map((item) => (
+        <div key={item.id}>
+          <h1>{item.email}</h1>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
