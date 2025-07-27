b7f5fd5a5256ab25ccc1a4a5e8e94c2ddb7b7683
Basit Ali <opoali911@gmail.com>
2025-07-27 19:27:21 +0500

    now start to work in server compoent

--- practice/src/app/albums/page.jsx
+++ practice/src/app/albums/page.jsx
@@ -1,11 +1,6 @@
 export default async function Albums() {
-    const response=await fetch("https://jsonplaceholder.typicode.com/albums")
-    const data=response.json();
-    
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  const data = response.json();
 
-    return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
