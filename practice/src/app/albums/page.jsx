9c04bf085901889e365dfb4debd7c720d63d6707
Basit Ali <opoali911@gmail.com>
2025-07-27 10:16:49 +0500

    create new routes and make a server component and there fetch data

---
+++ practice/src/app/albums/page.jsx
@@ -0,0 +1,11 @@
+export default async function Albums() {
+    const response=await fetch("https://jsonplaceholder.typicode.com/albums")
+    const data=response.json();
+    
+
+    return (
+    <div>
+      
+    </div>
+  )
+}
