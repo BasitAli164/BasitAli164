1cc2f6589a56cfdbff6033c442f3b01d3a08a622
Basit Ali <opoali911@gmail.com>
2025-07-28 23:12:40 +0500

    explore how to fetch multiple api in parallel

--- practice/src/app/all/page.js
+++ practice/src/app/all/page.js
@@ -1,14 +1,16 @@
 
 export default async function All() {
-    const all=await Promise.all([
+    const [post,comment,album]=await Promise.all([
         fetch("https://jsonplaceholder.typicode.com/posts?_limit=5"),
         fetch("https://jsonplaceholder.typicode.com/comments?_limit=5"),
-        fetch("https://jsonplaceholder.typicode.com/albums"),
+        fetch("https://jsonplaceholder.typicode.com/albums?_limit=5"),
         // as you wish
         
     ])
-    // const data=await all.json()
-    console.log("All :",all)
+   
+
+    const [postData,commentData,albumData]=await Promise.all([post.json(),comment.json(),album.json()])
+    console.log("Post Data: ",postData,"\n","Comment Data: ",commentData,'\n',"AlbumData: ",albumData)
   return (
     <div>
       
