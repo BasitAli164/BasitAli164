2d53d1f49f6f01e0cc4b16402a2c61321f4b87f2
Basit Ali <opoali911@gmail.com>
2025-07-28 23:05:22 +0500

    write a mini code for exploreing the parallel api fetching

--- practice/src/app/all/page.js
+++ practice/src/app/all/page.js
@@ -1,9 +1,13 @@
 
 export default async function All() {
     const all=await Promise.all([
-        fetch("")
+        fetch("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+        fetch("https://jsonplaceholder.typicode.com/comments?_limit=5"),
+        fetch("https://jsonplaceholder.typicode.com/albums"),
+        // as you wish
         
     ])
+    console.log("All :",alls)
   return (
     <div>
       
