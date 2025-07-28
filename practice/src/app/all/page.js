687a3693c018909a0169126ef9a8028f70850f4e
Basit Ali <opoali911@gmail.com>
2025-07-28 23:26:33 +0500

    now optimize the code of parallel fetching api

--- practice/src/app/all/page.js
+++ practice/src/app/all/page.js
@@ -1,16 +1,31 @@
 
-export default async function All() {
+// export default async function All() {
 
-    // Parallel Api Fetching concept or Technique or Method
-    const [post,comment,album]=await Promise.all([
-        fetch("https://jsonplaceholder.typicode.com/posts?_limit=5"),
-        fetch("https://jsonplaceholder.typicode.com/comments?_limit=5"),
-        fetch("https://jsonplaceholder.typicode.com/albums?_limit=5"),
-        // as you wish
+//     // Parallel Api Fetching concept or Technique or Method
+//     const [post,comment,album]=await Promise.all([
+//         fetch("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+//         fetch("https://jsonplaceholder.typicode.com/comments?_limit=5"),
+//         fetch("https://jsonplaceholder.typicode.com/albums?_limit=5"),
+//         // as you wish
         
-    ])
-   const [postData,commentData,albumData]=await Promise.all([post.json(),comment.json(),album.json()])
-    console.log("Post Data: ",postData,"\n","Comment Data: ",commentData,'\n',"AlbumData: ",albumData)
+//     ])
+//    const [postData,commentData,albumData]=await Promise.all([post.json(),comment.json(),album.json()])
+//     console.log("Post Data: ",postData,"\n","Comment Data: ",commentData,'\n',"AlbumData: ",albumData)
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+
+// Optimize code
+
+const fetchData=async(url)=>{
+    const res=await fetch(url)
+    return await res.json()
+}
+export default function page() {
   return (
     <div>
       
