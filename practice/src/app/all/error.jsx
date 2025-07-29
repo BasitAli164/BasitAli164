09ce5052397c1ff0c4949e166c8c4a1e7afb4df1
Basit Ali <opoali911@gmail.com>
2025-07-29 19:21:07 +0500

    explore the reset params which give the error in nextjs

--- practice/src/app/all/error.jsx
+++ practice/src/app/all/error.jsx
@@ -1,5 +1,5 @@
 'use client'
-export default function Error({error}) {
+export default function Error({error,reset}) {
   console.dir(error)
   console.log("Digest Value: ",error.digest)
   return (
@@ -9,6 +9,12 @@ export default function Error({error}) {
         <p>Message: {error.message}</p> 
         <p>Stack: {error.stack}</p>
         <p>Digest: {error.digest}</p>
+        <button
+        onClick={() => reset()}
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-400 cursor-pointer"
+      >
+        Try Again
+      </button>
     </div>
   )
 }
